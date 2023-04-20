1d2648dccaa15f07d7f8ca17ec37d70c6da30254
mlmanale <97977951+mlmanale@users.noreply.github.com>
2023-04-19 21:28:02 -0500

    started adding gamestates

--- sketch.js
+++ sketch.js
@@ -1,7 +1,32 @@
+let gameStart = true;
+
 function setup() {
   createCanvas(900, 600);
+  textAlign(CENTER);
 }
 
 function draw() {
   background(220);
+  if(gameStart) {
+    fill("black");
+    text("Press enter to begin!", width/2, height/2);
+  }
+
+  if(!gameStart) {
+    fill("black");
+    textSize(30);
+    text("GigaBite Cafe", width/2, 140);
+    
+    textSize(20);
+
+  }
 }
+
+function keyPressed() {
+  if(keyCode === ENTER && gameStart) {
+      console.log("gamestart works");
+      gameStart = false;
+  }
+}
+
+
